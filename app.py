e0295b4c5e5a703bd99015576cd173c327ef93e4
Aziz Chikh <azizchpay99@outlook.com>
2023-09-29 15:23:12 +0200

    Implemented the ChatBot SUCCESSFULLY

--- app.py
+++ app.py
@@ -1,5 +1,27 @@
 import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from decouple import config
 
+
+prompt = PromptTemplate(
+    input_variables=["chat_history", "question"],
+    template="""You are a kind AI agent and you are currently talking to a human. Answer in a friendly way and have a sense of humour
+                
+                chat_history: {chat_history}
+                
+                Human: {question}"""
+)
+
+llm = ChatOpenAI(openai_api_key=config("OPENAI_API_KEY"))
+memory = ConversationBufferWindowMemory(memory_key="chat_history", k=3)
+llm_chain = LLMChain(
+    llm=llm,
+    memory=memory,
+    prompt=prompt
+)
 
 st.set_page_config(
     page_title="ChatGPT Clone",
@@ -28,3 +50,13 @@ if user_prompt is not None:
     )
     with st.chat_message("user"):
         st.write(user_prompt)
+    
+    if st.session_state.messages[-1]["role"] == "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Loading ..."):
+                ai_response = llm_chain.predict(question=user_prompt)
+                st.write(ai_response)
+        new_ai_message = {"role": "assistant",
+         "content": ai_response}
+    
+        st.session_state.messages.append(new_ai_message)
